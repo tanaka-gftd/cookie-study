18242e9f588cf8ef247b98955fdd3e1121034549
Tanaka <f.tanaka@gftd.works>
2022-09-08 11:15:41 +0900

    学習のために、現在時刻（ミリ秒を）Cookieとしてセットし、webページに表示させるようにした

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('hi');
+
+  //現在時刻のミリ秒を取得して変数に格納
+  const now = Date.now();
+
+  //現在時刻（ミリ秒）をCookieとして、last_accessというキー名でヘッダにセット
+  /* セットされたCookieは、デベロッパーツール内のApplicationタブのCookiesという項目で確認できる */
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+
+  //ブラウザに保存されたCookieを、webページ(ここではlocalhost:8000)に表示させる
+  res.end(req.headers.cookie);
 });
 
 const port = 8000;
